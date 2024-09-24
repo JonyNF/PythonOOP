1bb3e452d0f03b4901034e6761160d8f0f6674b6
JonyNF <jonynfores@gmail.com>
2024-09-24 13:32:22 +0200

    Ch4: Inheritance Dragons #13

--- age_of_dragons.py
+++ age_of_dragons.py
@@ -1,3 +1,29 @@
+def main():
+    dragons = [
+        Dragon("Green Dragon", 0, 0, 1),
+        Dragon("Red Dragon", 2, 2, 2),
+        Dragon("Blue Dragon", 4, 3, 3),
+        Dragon("Black Dragon", 5, -1, 4),
+    ]
+
+    # don't touch above this line
+
+    for d in dragons:
+        describe(d)
+    
+    for d in dragons:
+        other_dragons = [t for t in dragons if t != d]
+        d.breathe_fire(3,3, other_dragons.copy())
+        
+
+
+# don't touch below this line
+
+
+def describe(dragon):
+    print(f"{dragon.name} is at {dragon.pos_x}/{dragon.pos_y}")
+
+
 class Unit:
     def __init__(self, name, pos_x, pos_y):
         self.name = name
@@ -5,10 +31,12 @@ class Unit:
         self.pos_y = pos_y
 
     def in_area(self, x_1, y_1, x_2, y_2):
-        if x_1 <= self.pos_x <= x_2 and y_1 <= self.pos_y <= y_2:
-            return True
-        else: 
-            return False
+        return (
+            self.pos_x >= x_1
+            and self.pos_x <= x_2
+            and self.pos_y >= y_1
+            and self.pos_y <= y_2
+        )
 
 
 class Dragon(Unit):
@@ -17,100 +45,15 @@ class Dragon(Unit):
         self.__fire_range = fire_range
 
     def breathe_fire(self, x, y, units):
-        blasted_units = []
-        min_x, max_x = x - self.__fire_range, x + self.__fire_range
-        min_y, max_y = y - self.__fire_range, y + self.__fire_range
-        
+        print(f"{self.name} breathes fire at {x}/{y} with range {self.__fire_range}")
+        print("====================================")
         for unit in units:
-            if unit.in_area(min_x, min_y, max_x, max_y):
-                blasted_units.append(unit)
-        return blasted_units
-    
-
-run_cases = [
-    (
-        [Unit("Cian", 3, 3), Unit("Andrew", -1, 4), Unit("Baran", -6, 5)],
-        Dragon("Draco", 2, 2, 3),
-        2,
-        3,
-        ["Cian", "Andrew"],
-    ),
-]
-
-submit_cases = run_cases + [
-    (
-        [
-            Unit("Carbry", 2, 1),
-            Unit("Yvor", 1, 0),
-            Unit("Eoin", 2, 0),
-            Unit("Edwin", 10, 10),
-        ],
-        Dragon("Fafnir", 1, 1, 1),
-        1,
-        1,
-        ["Carbry", "Yvor", "Eoin"],
-    ),
-    (
-        [Unit("Nicholas", 0, 1), Unit("Andrew", -1, 4), Unit("Baran", -6, 5)],
-        Dragon("Hydra", 0, 0, 2),
-        0,
-        1,
-        ["Nicholas"],
-    ),
-    (
-        [
-            Unit("Yvor", 1, 0),
-            Unit("Nicholas", 0, 1),
-            Unit("Eoin", 2, 0),
-            Unit("Cian", 3, 3),
-            Unit("Andrew", -1, 4),
-            Unit("Baran", -6, 5),
-            Unit("Carbry", 2, 1),
-        ],
-        Dragon("Smaug", 6, 6, 2),
-        1,
-        1,
-        ["Yvor", "Nicholas", "Eoin", "Cian", "Carbry"],
-    ),
-]
-
-
-def test(units, dragon, x_target, y_target, expected_hit_units):
-    print("---------------------------------")
-    print(f"Testing Dragon {dragon.name} at ({dragon.pos_x}, {dragon.pos_y})")
-    for unit in units:
-        print(f"Unit {unit.name} at ({unit.pos_x}, {unit.pos_y})")
-    print(f"Breathing fire at ({x_target}, {y_target})")
-    print(f"Expecting hit units: {expected_hit_units}")
-    hit_units = dragon.breathe_fire(x_target, y_target, units)
-    hit_unit_names = [unit.name for unit in hit_units]
-    print(f"Actual hit units: {hit_unit_names}")
-    if set(hit_unit_names) == set(expected_hit_units):
-        print("Pass")
-        return True
-    else:
-        print("Fail")
-        return False
-
-
-def main():
-    passed = 0
-    failed = 0
-    for test_case in test_cases:
-        correct = test(*test_case)
-        if correct:
-            passed += 1
-        else:
-            failed += 1
-    if failed == 0:
-        print("============= PASS ==============")
-    else:
-        print("============= FAIL ==============")
-    print(f"{passed} passed, {failed} failed")
-
-
-test_cases = submit_cases
-if "__RUN__" in globals():
-    test_cases = run_cases
-
+            in_area = unit.in_area(
+                x - self.__fire_range,
+                y - self.__fire_range,
+                x + self.__fire_range,
+                y + self.__fire_range,
+            )
+            if in_area:
+                print(f"{unit.name} is hit by the fire")
 main()
